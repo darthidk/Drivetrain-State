1181d50b2ad5495404d2da6b806c23f2dc1c40b1
darthidk <lochielion@gmail.com>
2025-12-01 23:50:21 +1100

    Fixed errors

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,64 @@
-#[derive(Default)]
+const NUM_MOTOR: usize = 4;
+
 enum GlobalState {
-    #[default]
-    Drive(vel: i32, angle: i32),
-    CrabWalk(vel: i32, angle: i32),
+    Drive {vel: i32, angle: i32},
+    CrabWalk {vel: i32, angle: i32},
 }
 
+impl Default for GlobalState {
+    fn default() -> Self {
+        GlobalState::Drive{vel: 0, angle: 0}
+    }
+}
+
+#[derive(Default)]
+struct MotorState {
+    vel: i32,
+    angle: i32,
+}
+
+
 impl GlobalState {
-    fn update(&mut self, magnitude: i32, angle: i32, swap_mode: bool) {
-        if (swap_mode) {
-            self = match self {
-                GlobalState::Drive => {
-                    GlobalState::CrabWalk(magnitude, angle);
+    fn update(&mut self, motors: &mut [MotorState; NUM_MOTOR], magnitude: i32, angle: i32, swap_mode: bool) {
+        if swap_mode {
+            let new_state = match self {
+                GlobalState::Drive{vel: _, angle: _} => {
+                    GlobalState::CrabWalk{vel: magnitude, angle: angle}
                 },
-                GlobalState::CrabWalk => {
-                    GlobalState::Drive(magnitude, angle);
+                GlobalState::CrabWalk{vel: _, angle: _} => {
+                    GlobalState::Drive{vel: magnitude, angle: angle}
                 },
+            };
+            *self = new_state;
         } else {
-            self = match self {
-                GlobalState::Drive => {
-                    GlobalState::Drive(magnitude, angle);
+            *self = match self {
+                GlobalState::Drive{vel: _, angle: _} => {
+                    GlobalState::Drive{vel: magnitude, angle: angle}
                 },
-                GlobalState::CrabWalk => {
-                    GlobalState::CrabWalk(magnitude, angle);
+                GlobalState::CrabWalk{vel: _, angle: _} => {
+                    GlobalState::CrabWalk{vel: magnitude, angle: angle}
+                },
+            };
+            match self {
+                GlobalState::Drive{vel: _, angle: _} => {
+                    for i in 0..2 {
+                        motors[i] = MotorState{vel: magnitude, angle: angle};
+                    }
+                    for i in 2..4 {
+                        motors[i] = MotorState{vel: magnitude, angle: 0};
+                    }
+                }
+                GlobalState::CrabWalk{vel: _, angle:_} => {
+                    for i in 0..NUM_MOTOR {
+                        motors[i] = MotorState{vel: magnitude, angle: angle};
+                    }
+                }
             }
         }
     }
 }
 
+
 fn main() {
+
 }
